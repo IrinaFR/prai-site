4740111754cf13679c31e00296d58a0152011808
IrinaFR <irina.chernova2016@mail.ru>
2023-09-18 08:12:26 +0300

    Блок Кейсов

---
+++ components/ui/PraiUiCardCase.vue
@@ -0,0 +1,109 @@
+<template lang="pug">
+nuxt-link.card-case(to="/")
+	.card-case-image
+		img(src="/img/test.png")
+	.card-case-info
+		.title Корпоративный сайт для застройщика с админ-панелью и интеграцией
+		.list-services
+			.item
+				span.bracket [
+				|  Разработка веб-приложений
+				span.bracket  ]
+		.list-finals
+			.subtitle-card.light-pink Корпоративный сайт
+			.subtitle-card.light-pink Админ-панель
+			.subtitle-card.light-pink Интеграция
+			.subtitle-card.light-pink SEO
+</template>
+
+<style lang="scss">
+	.card-case{
+		display: block;
+		border-radius: 24px;
+		background: $bg-block-black;
+		border: solid 1px $dark-border;
+		max-width: 534px;
+		width: 100%;
+		min-height: 504px;
+		overflow: hidden;
+		position: relative;
+		z-index: 1;
+		flex-grow: 1;
+		&:hover{
+			.title{
+				color: $blue;
+			}
+		}
+		@media(max-width: 768px){
+			max-width: 100%;
+		}
+		@media(max-width: 600px) {
+			min-height: fit-content;
+		}
+		&-image{
+			width: 100%;
+			height: 310px;
+			@media(max-width: 930px){
+				height: 260px;
+			}
+			@media(max-width: 760px){
+				height: 220px;
+			}
+			@media(max-width: 600px){
+				height: 330px;
+			}
+			@media(max-width: 495px){
+				height: 290px;
+			}
+			@media(max-width: 410px){
+				height: 230px;
+			}
+			@media(max-width: 345px){
+				height: 190px;
+			}
+			img{
+				width: 100%;
+				height: 100%;
+				object-fit: cover;
+				pointer-events: none;
+			}
+		}
+		&-info{
+			padding: 30px;
+			width: calc(100% - 60px);
+			@media(max-width: 600px) {
+				padding: 20px;
+				width: calc(100% - 40px);
+			}
+			.title{
+				font-size: 19px;
+				font-weight: 600;
+				color: $white;
+				margin-bottom: 16px;
+				line-height: 140%;
+				transition: $anim-small;
+			}
+			.list-services{
+				display: flex;
+				align-items: center;
+				flex-wrap: wrap;
+				gap: 8px 16px;
+				margin-bottom: 16px;
+				.item{
+					font-size: 14px;
+					color: $dark-secondary-text;
+					span.bracket{
+						font-weight: 700;
+						// TODO Добавить цвет
+						color: $light-pink;
+					}
+				}
+			}
+			.list-finals{
+				display: flex;
+				flex-wrap: wrap;
+				gap: 8px 16px;
+			}
+		}
+	}
+</style>
---
+++ components/ui/PraiUiCases.vue
@@ -0,0 +1,136 @@
+<template lang="pug">
+.bg-black
+	.container
+		.row-cases
+			.gradient_1
+			.gradient_2
+			.col-cases
+				.title-block.white
+					.subtitle-card.blue.uppercase Кейсы
+					h2.title Выполненные проекты
+				PraiUiCardCase
+				.card-feedback(v-if="$store.getters.getWidth > 600")
+					.title Обсудить проект или пригласить в тендер
+					.description Оставьте номер телефона и мы вам перезвоним
+					form
+						input(placeholder="+7 999 000-00-00")
+						PraiUiButtons.btn-full(text="Связаться с нами" full)
+						label.loadFile(for="feedbackPinFile")
+							input.d-none#feedbackPinFile(type="file")
+							img(src="/img/form/document.svg")
+							| Прикрепить файл
+						.tip Отправляя форму, вы соглашаетесь с
+							a(href="/") политикой конфинденциальности
+			.col-cases
+				PraiUiCardCase
+				PraiUiCardCase
+				.card-feedback.mob
+					.title Обсудить проект или пригласить в тендер
+					.description Оставьте номер телефона и мы вам перезвоним
+					form
+						input(placeholder="+7 999 000-00-00")
+						PraiUiButtons.btn-full(text="Связаться с нами" full)
+						label.loadFile(for="feedbackPinFile")
+							input.d-none#feedbackPinFile(type="file")
+							img(src="/img/form/document.svg")
+							| Прикрепить файл
+						.tip Отправляя форму, вы соглашаетесь с
+							a(href="/") политикой конфинденциальности
+		PraiUiButtons.mx-auto.mt-40(text="Смотреть все кейсы" )
+
+</template>
+
+<script>
+	import PraiUiCardCase from '/components/ui/PraiUiCardCase.vue'
+	import PraiUiButtons from '/components/ui/PraiUiButtons.vue'
+	export default {
+		components: { PraiUiCardCase, PraiUiButtons }
+	}
+</script>
+
+<style scoped lang="scss">
+	.row-cases{
+		display: flex;
+		gap: 16px;
+		position: relative;
+		@media(max-width: 600px){
+			flex-direction: column;
+		}
+		.col-cases{
+			display: flex;
+			flex-direction: column;
+			row-gap: 16px;
+		}
+		.gradient_1 {
+			position: absolute;
+			width: 270px;
+			background: linear-gradient(180deg, #073C8A 0%, #7644B6 100%);
+			filter: blur(44px);
+			height: 530px;
+			transform: rotate(306deg);
+			left: auto;
+			right: 20%;
+			top: 160px;
+			border-top-left-radius: 150px;
+			border-bottom-right-radius: 350px;
+			z-index: 0;
+			@media(max-width: 768px) {
+				display: none;
+			}
+		}
+		.gradient_2 {
+			position: absolute;
+			width: 270px;
+			background: linear-gradient(180deg, #073C8A 0%, #7644B6 100%);
+			filter: blur(44px);
+			height: 530px;
+			transform: rotate(256deg);
+			left: 25%;
+			bottom: -170px;
+			border-top-left-radius: 150px;
+			border-bottom-right-radius: 350px;
+			z-index: 0;
+			@media(max-width: 768px) {
+				display: none;
+			}
+		}
+	}
+	.card-feedback{
+		flex-grow: 1;
+		padding: 30px;
+		background: $bg-block-black;
+		border-radius: $radius-small;
+		border: solid 1px $dark-border;
+		display: flex;
+		flex-direction: column;
+		row-gap: 12px;
+		position: relative;
+		z-index: 1;
+		&.mob{
+			display: none;
+			padding: 20px;
+			@media(max-width: 601px){
+				display: flex;
+			}
+		}
+		.title{
+			font-size: 19px;
+			font-weight: 600;
+			color: $white;
+		}
+		.description{
+			flex-grow: 1;
+			font-size: 14px;
+			color: $dark-secondary-text;
+		}
+		form {
+			input{
+				width: calc(100% - 32px);
+			}
+			.btn-full{
+				width: 100%;
+				margin: 16px 0;
+			}
+		}
+	}
+</style>
